62b3945c44df78e8e6a927c3c62961797b117a27
fgladu <fgladu@gmail.com>
2023-11-14 17:47:20 -0500

    Update node_exporter_amd64-cron.sh

--- node_exporter_amd64-cron.sh
+++ node_exporter_amd64-cron.sh
@@ -6,6 +6,20 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Function to get the installed version
+get_installed_version() {
+    sudo /usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
+}
+
+# Check the currently installed version
+INSTALLED_VERSION=$(get_installed_version)
+
+# Exit if unable to determine the installed version
+if [ -z "$INSTALLED_VERSION" ]; then
+    echo "Failed to determine the currently installed version."
+    exit 1
+fi
+
 # Fetch the latest version using GitHub API
 LATEST_VERSION=$(curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")')
 
@@ -18,16 +32,13 @@ fi
 # Set the downloaded version to the latest version without the "v"
 VERSION="${LATEST_VERSION#v}"
 
-# Check the currently installed version
-INSTALLED_VERSION=$(node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)')
-
-# Compare the installed version with the latest version
+# Compare the GitHub version with the installed version
 if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
     echo "node_exporter is already up-to-date (version $INSTALLED_VERSION). Exiting."
     exit 0
 fi
 
-echo "Updating node_exporter to version $VERSION."
+echo "Updating node_exporter from version $INSTALLED_VERSION to version $VERSION."
 
 # Rest of the script remains the same...
 
@@ -62,4 +73,4 @@ curl -X POST -H "Content-Type: application/json" -d '{"text": "Failed to start n
 # Exit with an error code
 exit 1
 
-# Version 2023-11-14_17:30:00
+# Version 2023-11-14_17:47:00
