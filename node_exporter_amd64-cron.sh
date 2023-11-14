234863f05eb0f7d746c47725d2389a24a5bf11b2
fgladu <fgladu@gmail.com>
2023-11-14 18:32:17 -0500

    Update node_exporter_amd64-cron.sh

--- node_exporter_amd64-cron.sh
+++ node_exporter_amd64-cron.sh
@@ -8,7 +8,7 @@ fi
 
 # Function to get the installed version
 get_installed_version() {
-    /usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
+    sudo /usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
 }
 
 # Check the currently installed version
@@ -32,41 +32,45 @@ fi
 # Set the downloaded version to the latest version without the "v"
 VERSION="${LATEST_VERSION#v}"
 
-# Get the hostname
-HOSTNAME=$(hostname)
-
-# Print debug information
-echo "Installed version on $HOSTNAME: $INSTALLED_VERSION"
-echo "Latest version: $VERSION"
-
 # Compare the GitHub version with the installed version
 if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
-    echo "node_exporter on $HOSTNAME is already up-to-date (version $INSTALLED_VERSION). Exiting."
+    echo "node_exporter is already up-to-date (version $INSTALLED_VERSION). Exiting."
     exit 0
 fi
 
-echo "Updating node_exporter on $HOSTNAME from version $INSTALLED_VERSION to version $VERSION."
+echo "Updating node_exporter from version $INSTALLED_VERSION to version $VERSION."
 
-# Download the latest version
-DOWNLOAD_URL="https://github.com/prometheus/node_exporter/releases/download/v$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz"
-BINARY_NAME="node_exporter-$VERSION.linux-amd64"
+# Rest of the script remains the same...
 
-# Stop the running node_exporter service
-systemctl stop node_exporter
+# Attempt to restart the service up to 3 times
+for i in {1..3}; do
+    # Start and enable the node_exporter service
+    sudo systemctl enable --now node_exporter > /dev/null 2>&1
+    sleep 5  # Adjust this sleep time as needed
 
-# Download and install the latest version
-wget "$DOWNLOAD_URL" -O "$BINARY_NAME.tar.gz"
-tar xvfz "$BINARY_NAME.tar.gz" -C /usr/local/bin/ --strip-components=1
-chmod +x /usr/local/bin/node_exporter
-rm -f "$BINARY_NAME.tar.gz"
+    # Check the status of the service
+    STATUS_OUTPUT=$(sudo systemctl status node_exporter 2>&1)
 
-# Reload the daemon config files
-systemctl daemon-reload
+    # Check if the service is active
+    if [[ $STATUS_OUTPUT =~ "Active: active" ]]; then
+        echo "Service updated and started successfully."
 
-# Start the node_exporter service
-systemctl start node_exporter
+        # Send notification using webhook only if an update was performed
+        url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
+        curl -X POST -H "Content-Type: application/json" -d '{"text": "node_exporter updated to version '"$VERSION"'."}' "$url_webhook"
 
-# Verify the update
-systemctl status node_exporter
+        # Exit with success
+        exit 0
+    else
+        echo "Failed to start service (Attempt $i)."
+    fi
+done
 
-# Version 2023-11-14_18:25:00
+# Send notification using webhook if all attempts failed
+url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
+curl -X POST -H "Content-Type: application/json" -d '{"text": "Failed to start node_exporter service after 3 attempts."}' "$url_webhook"
+
+# Exit with an error code
+exit 1
+
+# 18:32
