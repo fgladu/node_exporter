ea0c6edabf16eb706db3a926e848de4ad20b7678
fgladu <fgladu@gmail.com>
2023-11-14 19:01:31 -0500

    Update node_exporter_amd64-cron.sh

--- node_exporter_amd64-cron.sh
+++ node_exporter_amd64-cron.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Get the hostname
-HOSTNAME=$(hostname)
-
-# Compare the GitHub version with the installed version
-if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
-    echo "node_exporter on $HOSTNAME is already up-to-date (version $INSTALLED_VERSION). Exiting."
-    exit 0
-fi
-
-echo "Updating node_exporter on $HOSTNAME from version $INSTALLED_VERSION to version $VERSION."
-
 # Check if running as root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root." 1>&2
@@ -22,12 +11,15 @@ get_installed_version() {
     sudo /usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
 }
 
+# Get the hostname
+HOSTNAME=$(hostname)
+
 # Check the currently installed version
 INSTALLED_VERSION=$(get_installed_version)
 
 # Exit if unable to determine the installed version
 if [ -z "$INSTALLED_VERSION" ]; then
-    echo "Failed to determine the currently installed version."
+    echo "Failed to determine the currently installed version on $HOSTNAME."
     exit 1
 fi
 
@@ -36,15 +28,12 @@ LATEST_VERSION=$(curl -s https://api.github.com/repos/prometheus/node_exporter/r
 
 # Exit if the latest version couldn't be fetched
 if [ -z "$LATEST_VERSION" ]; then
-    echo "Failed to fetch the latest version from GitHub API."
+    echo "Failed to fetch the latest version from GitHub API on $HOSTNAME."
     exit 1
 fi
 
 # Set the downloaded version to the latest version without the "v"
 VERSION="${LATEST_VERSION#v}"
-
-# Get the hostname
-HOSTNAME=$(hostname)
 
 # Compare the GitHub version with the installed version
 if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
@@ -53,8 +42,6 @@ if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
 fi
 
 echo "Updating node_exporter on $HOSTNAME from version $INSTALLED_VERSION to version $VERSION."
-
-# Rest of the script remains the same...
 
 # Attempt to restart the service up to 3 times
 for i in {1..3}; do
@@ -67,24 +54,24 @@ for i in {1..3}; do
 
     # Check if the service is active
     if [[ $STATUS_OUTPUT =~ "Active: active" ]]; then
-        echo "Service updated and started successfully."
+        echo "Service on $HOSTNAME updated and started successfully."
 
         # Send notification using webhook only if an update was performed
         url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-        curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"node_exporter on $HOSTNAME updated to version $VERSION.\"}" "$url_webhook"
+        curl -X POST -H "Content-Type: application/json" -d '{"text": "node_exporter on '"$HOSTNAME"' updated to version '"$VERSION"'."}' "$url_webhook"
 
         # Exit with success
         exit 0
     else
-        echo "Failed to start service (Attempt $i)."
+        echo "Failed to start service on $HOSTNAME (Attempt $i)."
     fi
 done
 
 # Send notification using webhook if all attempts failed
 url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"Failed to start node_exporter service on $HOSTNAME after 3 attempts.\"}" "$url_webhook"
+curl -X POST -H "Content-Type: application/json" -d '{"text": "Failed to start node_exporter service on '"$HOSTNAME"' after 3 attempts."}' "$url_webhook"
 
 # Exit with an error code
 exit 1
 
-# 18:54
+# 19:01
