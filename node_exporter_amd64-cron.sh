863b073d3d6afd4cb03e4927d71f08338689c884
fgladu <fgladu@gmail.com>
2023-11-15 10:49:50 -0500

    Update node_exporter_amd64-cron.sh

--- node_exporter_amd64-cron.sh
+++ node_exporter_amd64-cron.sh
@@ -20,13 +20,16 @@ VERSION="${LATEST_VERSION#v}"
 # Check the currently installed version
 INSTALLED_VERSION=$(get_installed_version)
 
+# Get the hostname
+HOSTNAME=$(hostname)
+
 # Compare the GitHub version with the installed version
 if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
-	echo "node_exporter is already up-to-date (version $INSTALLED_VERSION). Exiting."
+	echo "$HOSTNAME - node_exporter is already up-to-date (version $INSTALLED_VERSION). Exiting."
 	exit 0
 fi
 
-echo "Updating node_exporter from version $INSTALLED_VERSION to version $VERSION."
+echo "$HOSTNAME - Updating node_exporter from version $INSTALLED_VERSION to version $VERSION."
 
 # Attempt to restart the service up to 3 times
 for i in {1..3}; do
@@ -39,22 +42,22 @@ for i in {1..3}; do
 
 	# Check if the service is active
 	if [[ $STATUS_OUTPUT =~ "Active: active" ]]; then
-		echo "Service updated and started successfully."
+		echo "$HOSTNAME - Service updated and started successfully."
 
 		# Send notification using webhook only if an update was performed
 		url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-		curl -X POST -H "Content-Type: application/json" -d '{"text": "node_exporter updated to version '"$VERSION"'."}' "$url_webhook"
+		curl -X POST -H "Content-Type: application/json" -d '{"text": "'"$HOSTNAME"' - node_exporter updated to version '"$VERSION"'."}' "$url_webhook"
 
 		# Exit with success
 		exit 0
 	else
-		echo "Failed to start service (Attempt $i)."
+		echo "$HOSTNAME - Failed to start service (Attempt $i)."
 	fi
 done
 
 # Send notification using webhook if all attempts failed
 url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-curl -X POST -H "Content-Type: application/json" -d '{"text": "Failed to start node_exporter service after 3 attempts."}' "$url_webhook"
+curl -X POST -H "Content-Type: application/json" -d '{"text": "'"$HOSTNAME"' - Failed to start node_exporter service after 3 attempts."}' "$url_webhook"
 
 # Exit with an error code
 exit 1
