83fe22c98464ecd8a9b0834ad6ad5ab12e439c03
fgladu <fgladu@gmail.com>
2023-11-15 15:58:12 -0500

    Update node_exporter_amd64-cron.sh

--- node_exporter_amd64-cron.sh
+++ node_exporter_amd64-cron.sh
@@ -2,7 +2,7 @@
 
 # Fonction pour obtenir la version installée
 get_installed_version() {
-    /usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
+	/usr/local/bin/node_exporter --version 2>/dev/null | grep -oP 'version \K(.*?)(?=\s)'
 }
 
 # Récupérer la dernière version en utilisant l'API GitHub
@@ -10,8 +10,8 @@ LATEST_VERSION=$(curl -s https://api.github.com/repos/prometheus/node_exporter/r
 
 # Sortir si la dernière version n'a pas pu être récupérée
 if [ -z "$LATEST_VERSION" ]; then
-    echo "Échec de récupération de la dernière version depuis l'API GitHub."
-    exit 1
+	echo "Échec de récupération de la dernière version depuis l'API GitHub."
+	exit 1
 fi
 
 # Définir la version téléchargée comme la dernière version sans le "v"
@@ -22,8 +22,8 @@ INSTALLED_VERSION=$(get_installed_version)
 
 # Comparer la version GitHub avec la version installée
 if [ "$INSTALLED_VERSION" == "$VERSION" ]; then
-    echo "node_exporter est déjà à jour (version $INSTALLED_VERSION) sur $(hostname). Sortie."
-    exit 0
+	echo "node_exporter est déjà à jour (version $INSTALLED_VERSION) sur $(hostname). Sortie."
+	exit 0
 fi
 
 echo "Mise à jour de node_exporter de la version $INSTALLED_VERSION à la version $VERSION sur $(hostname)."
@@ -53,29 +53,29 @@ sudo systemctl restart node_exporter
 
 # Vérifier la mise à jour
 if [ "$(get_installed_version)" == "$VERSION" ]; then
-    echo "Service mis à jour et démarré avec succès."
+	echo "Service mis à jour et démarré avec succès."
 
-    # Envoyer une notification en utilisant le webhook
-    url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-    MESSAGE="$(hostname) - node_exporter mis à jour à la version $VERSION."
-    ESCAPED_MESSAGE=$(echo "$MESSAGE" | sed 's/"/\\"/g')
-    JSON_PAYLOAD="{\"text\": \"$ESCAPED_MESSAGE\"}"
-    curl -X POST -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$url_webhook"
+	# Envoyer une notification en utilisant le webhook
+	url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
+	MESSAGE="node_exporter mis à jour à la version $VERSION sur $(hostname)."
+	ESCAPED_MESSAGE=$(echo "$MESSAGE" | sed 's/"/\\"/g')
+	JSON_PAYLOAD="{\"text\": \"$ESCAPED_MESSAGE\"}"
+	curl -X POST -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$url_webhook"
 
-    # Sortir avec succès
-    exit 0
+	# Sortir avec succès
+	exit 0
 else
-    echo "Échec de la mise à jour du service. Sortie."
+	echo "Échec de la mise à jour du service. Sortie."
 
-    # Envoyer une notification en utilisant le webhook
-    url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
-    MESSAGE="Échec de la mise à jour de node_exporter sur $(hostname)."
-    ESCAPED_MESSAGE=$(echo "$MESSAGE" | sed 's/"/\\"/g')
-    JSON_PAYLOAD="{\"text\": \"$ESCAPED_MESSAGE\"}"
-    curl -X POST -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$url_webhook"
+	# Envoyer une notification en utilisant le webhook
+	url_webhook="https://chat.googleapis.com/v1/spaces/AAAAhWiyzzE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=s0DeZk91_SvZdQAozzlhiCcgoKxmCu5nldP9TvlSbr4"
+	MESSAGE="Échec de la mise à jour de node_exporter sur $(hostname)."
+	ESCAPED_MESSAGE=$(echo "$MESSAGE" | sed 's/"/\\"/g')
+	JSON_PAYLOAD="{\"text\": \"$ESCAPED_MESSAGE\"}"
+	curl -X POST -H "Content-Type: application/json" -d "$JSON_PAYLOAD" "$url_webhook"
 
-    # Sortir avec un code d'erreur
-    exit 1
+	# Sortir avec un code d'erreur
+	exit 1
 fi
 
-# 15:55
+# 15:58
